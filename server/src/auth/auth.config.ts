21fa00a523b164d9662909ef8785b45c5b26ab23
Bramaroot <127478690+Bramaroot@users.noreply.github.com>
2025-11-27 11:17:42 +0100

    goodOne

--- server/src/auth/auth.config.ts
+++ server/src/auth/auth.config.ts
@@ -1,12 +1,16 @@
 if (!process.env.JWT_ACCESS_SECRET) {
-  throw new Error('FATAL_ERROR: JWT_ACCESS_SECRET is not defined in environment variables.');
+  throw new Error(
+    "FATAL_ERROR: JWT_ACCESS_SECRET is not defined in environment variables."
+  );
 }
 
 if (!process.env.JWT_REFRESH_SECRET) {
-  throw new Error('FATAL_ERROR: JWT_REFRESH_SECRET is not defined in environment variables.');
+  throw new Error(
+    "FATAL_ERROR: JWT_REFRESH_SECRET is not defined in environment variables."
+  );
 }
 
-export const ACCESS_TOKEN_TTL = "15m";
+export const ACCESS_TOKEN_TTL = "7d";
 export const REFRESH_TOKEN_TTL = "7d";
 export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
 export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
